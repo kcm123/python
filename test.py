bec2d7919ce4cb830c0a4501e964ac1742b48e54
name <chanmi5857@gmail.com>
2020-12-31 15:12:40 +0900

    Initial commit

---
+++ test.py
@@ -0,0 +1,12 @@
+def add_10(value):
+    if value < 40:
+        return 10
+    else:
+        result = value + 10
+        return result
+print(add_10(32))
+print("{} {} 테스트".format('2', '4'))
+print()
+test = [{'id':'cat'}, {'id':'dog'},{'id':'bird'} ]
+for t in test:
+    print(t.get('id'))
