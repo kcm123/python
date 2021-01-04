507c9a9b5bebd7fb8f6c484afd2ea57e2b39c0b5
name <chanmi5857@gmail.com>
2021-01-04 13:53:18 +0900

    자료형_문자열

---
+++ A_Basic/aDataType.py
@@ -0,0 +1,13 @@
+a = "승건이가 \"그만해\"라고 말했다"
+b = '승건\'s 뱃살'
+c = "python" + "java"
+d = "python" * 2
+print(c[0:6])
+e = "Python"
+print(a[:4]) #처음부터 a[3]까지
+print(a[2:4]) #a[2]부터 a[3]까지
+print(a[3:]) #a[3] 부터 마지막
+
+f = "I eat %d apples." %3
+g = "I eat %s apples" %"five"
+print(g)
